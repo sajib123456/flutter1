1700c72a18911241416effb2f9395867baaa2d39
sajib123456 <90611173+sajib123456@users.noreply.github.com>
2021-09-13 16:21:54 +0600

    Create row button.dart

---
+++ row button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child:
+                      ElevatedButton(onPressed: () {}, child: Text("Button1")),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(onPressed: () {}, child: Text("Button2")),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
